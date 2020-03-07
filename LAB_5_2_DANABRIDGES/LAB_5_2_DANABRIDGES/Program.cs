08d25954671ebc48ee306b70b55e8700b241f7ca
enocpi <61128286+enocpi@users.noreply.github.com>
2020-03-06 18:32:09 -0600

    Add files via upload

---
+++ LAB_5_2_DANABRIDGES/LAB_5_2_DANABRIDGES/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+
+namespace LAB_5_2_DANABRIDGES
+{
+    class Program
+
+
+
+        
+    { 
+        enum WeekDays
+        {   Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+
+        };
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            ArrayList tasklist = new ArrayList();
+           
+            foreach (WeekDays days in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine("enter task for" + days);
+                string task = days + Console.ReadLine();
+                tasklist.Add(task);
+
+            }
+
+
+            foreach (string task in tasklist)
+            {
+                Console.WriteLine(task);
+            }
+
+
+
+        }
+    }
+}
